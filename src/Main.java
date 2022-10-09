c6124925ebbbd53b10dc5b718c80ec3b20ea9ef5
bazlin111 <pbazlin@yandex.ru>
2022-10-09 14:31:20 +0400

    ДЗ Базлин П.Н. доработал .

--- src/Main.java
+++ src/Main.java
@@ -38,23 +38,21 @@ public class Main {
         int total2 = 0;
         int p = 0;
         for (; total2 < 12_000_000; p++) {
-            total2 = total2 + total2 *7/100;
+            total2 = total2 + total2 * 7 / 100;
             total2 = total2 + salary2;
-            //    if (p % 1  == 0)
-            {
-                System.out.println(" Месяц " + p + ", сумма накоплений равна " + total2 + " рублей ");
+            //    if (p % 1  == 0)}
+            {  System.out.println(" Месяц " + p + ", сумма накоплений равна " + total2 + " рублей ");
             }
         }
         System.out.println();
         System.out.println(" Задание №5");
-        int total3= 0;
+        int total3 = 0;
         int salary3 = 15_000;
         int s = 0;
-        for (; total3 < 12_000_000; s ++) {
-            total3 = total3+  total3 *7/100;
+        for (; total3 < 12_000_000; s++) {
+            total3 = total3 + total3 * 7 / 100;
             total3 = total3 + salary3;
-            if (s % 6  == 0)
-            {
+            if (s % 6 == 0) {
                 System.out.println(" Месяц " + s + ", сумма накоплений равна " + total3 + " рублей ");
             }
         }
@@ -64,22 +62,35 @@ public class Main {
         int total4 = 0;
         int mon = 0;
         while (mon <= 108) {
-            total4 = total4 + total4 *7/100;
+            total4 = total4 + total4 * 7 / 100;
             total4 = total4 + salary4;
             mon++;
             if (mon % 6 == 0) {
-                System.out.println (" Месяц " + mon + ", сумма накоплений равна " + total4 + " рублей ");
+                System.out.println(" Месяц " + mon + ", сумма накоплений равна " + total4 + " рублей ");
             }
         }
         System.out.println();
         System.out.println(" Задание № 7 ");
         int f = 1;
-        for (;f<=31; f+=7){
-            System.out.printf( " Сегодня пятница , %d -e число . Необходимо подготовить отчет .\n",f);
+        for (; f <= 31; f += 7) {
+            System.out.printf(" Сегодня пятница , %d -e число . Необходимо подготовить отчет .\n", f);
         }
-
-
+        System.out.println();
+        System.out.println(" Задание № 8 ");
+        int cometCycle = 2022;
+        int beginning = cometCycle - 200;
+        int ending = cometCycle + 100;
+        for (int j = beginning; j < ending; ++j) {
+            if (j % 79 == 0) {
+                System.out.println(j);
+            }
+        }
+        System.out.println();
+        System.out.println(" Задание № 9 ");
+        int result;
+        for (int h = 1; h <= 10; h++) {
+            result = h * 2;
+            System.out.println(" 2 * " + h + " = " + result);
+        }
     }
-
-
 }
